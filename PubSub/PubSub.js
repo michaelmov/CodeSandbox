e7040920a93783bc353f314987dcc74a08b59621
Michael Movsesov <michael.movsesov@gmail.com>
2018-03-21 19:11:30 -0700

    Added PubSub pattern.

---
+++ PubSub/PubSub.js
@@ -0,0 +1,40 @@
+class PubSub {
+    constructor() {
+        this.events = {}
+    }
+
+    on(eventName, fn) {
+        this.events[eventName] = this.events[eventName] || [];
+        this.events[eventName].push(fn);
+    }
+
+    off(eventName, fn) {
+        if(this.events[eventName]) {
+            for (var i = 0; i < this.events[eventName].length; i++) {
+                if (this.events[eventName][i] === fn) {
+                  this.events[eventName].splice(i, 1);
+                  break;
+                };
+            }
+        };
+    }
+
+    emit(eventName, data) {
+        if(this.events[eventName]) {
+            this.events[eventName].forEach(function(fn) { 
+                fn(data);
+            });
+        }
+    }
+}
+
+const pubSub = new PubSub();
+
+pubSub.on('click', console.log);
+
+
+pubSub.emit('click', 'Hello');
+pubSub.emit('click', 'Yo!');
+
+
+
